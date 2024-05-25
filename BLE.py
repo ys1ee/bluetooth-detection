9eb2332bdb6eafc2bc67cb0d9960ec70784a6f40
manchenlee <sleep1700505@gmail.com>
2024-05-25 18:17:40 +0800

    add authDeviceList.json

--- BLE.py
+++ BLE.py
@@ -1,46 +1,40 @@
 import asyncio
 from bleak import BleakScanner
 from utils import *
+import json
+import argparse
 
-def rssi_to_distance(rssi, A=-59, n=2):
-    if rssi == 0:
-        return -1.0  # if we cannot determine distance, return -1.
+AUTH_LIST_PATH = './private/authDeviceList.json'
 
-    distance = 10 ** ((A - rssi) / (10 * n))
-    return distance
+def get_auth_BLE_device():
+    try:
+        with open(AUTH_LIST_PATH, 'r') as f:
+            d = json.load(fp = f)
+        return d
+    except:
+        return {}
 
-def checkAuth(device):
-    while True:
-        try:
-            t = input('Is the BLE device authenticated? (0/1)')
-            if len(t) != 1 or (not t.isnumeric()):
-                #print(t)
-                raise ValueError('Please input 0(False) or 1(True)!!')
-            elif t.isnumeric() and (int(t) != 0 and int(t) != 1):
-                raise ValueError('Please input 0(False) or 1(True)!!')
-            else: 
-                break
-        except ValueError as msg:
-            print(msg)
-            continue
-    device.isAuth = int(t)
-    return device
+def update_auth_BLE_device(authDict):
+    json.dump(authDict, open(AUTH_LIST_PATH,'w'))
+    print('authDeviceList UPDATED !!')
 
 async def discover_devices():
+    authDict = get_auth_BLE_device()
     print("Scanning for nearby Bluetooth devices...")
     devices = await BleakScanner.discover(return_adv=True)
-
     print(f"Found {len(devices)} devices")
-
     for device, data in devices.values():
-        print(f"Addr: {device.address}, RSSI: {data.rssi} dBm, Dis: {rssi_to_distance(data.rssi)} m")
-        temp = BLEDevice(device.address)
-        temp = checkAuth(temp)
-        print(temp.isAuth)
-        # print(type(device), device)
-        # print(type(data), data)
-        # print()
+        company = None
+        temp = list(data.manufacturer_data.keys())
+        if len(temp) == 1:
+            company = temp[0]
+        new = BLEDevice(device.address, device.name, company)
+        new.printInfo(data.rssi)
+        authDict = new.checkAuth(authDict)
+    update_auth_BLE_device(authDict)
+        
 
 if __name__ == "__main__":
+    
     loop = asyncio.get_event_loop()
     loop.run_until_complete(discover_devices())
--- utils.py
+++ utils.py
@@ -1,4 +1,43 @@
+
+
+def rssi_to_distance(rssi, A=-59, n=2):
+        if rssi == 0:
+            return -1.0  # if we cannot determine distance, return -1.
+        distance = 10 ** ((A - rssi) / (10 * n))
+        return distance
+
 class BLEDevice():
-    def __init__(self, addr, isAuth=None):
+    def __init__(self, addr, name="", company=0, isAuth=None):
         self.addr = addr
+        self.name = name
+        self.company = company
         self.isAuth = isAuth
+    
+    def printInfo(self, rssi):
+        print(f"Addr: {self.addr}\n Name: {self.name}\n RSSI: {rssi} dBm\n Dis: {rssi_to_distance(rssi)} m")
+
+    def checkAuth(self, authDict):
+        if self.addr in authDict:
+            print("The BLE device \"{}\" has been in authDeviceList!!".format(self.addr))
+            self.isAuth = 1
+            return authDict
+        else:
+            while True:
+                try:
+                    t = input('Is the BLE device \"{}\" authenticated? (0/1)'.format(self.addr))
+                    if len(t) != 1 or not t.isnumeric():
+                        #print(t)
+                        raise ValueError('Error: Please input 0(False) or 1(True)!!')
+                    elif t.isnumeric() and (int(t) != 0 and int(t) != 1):
+                        raise ValueError('Error: Please input 0(False) or 1(True)!!')
+                    else: 
+                        break
+                except ValueError as msg:
+                    print(msg)
+                    continue
+            isAuth = int(t)
+            self.isAuth = isAuth
+            if isAuth:
+                authDict[self.addr] = {"Name": self.name, "Company": self.company}
+            return authDict
+            
